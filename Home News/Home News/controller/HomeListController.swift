35189c4584207d7caceeba379572417c922783c1
san2ride <sanchezjason@me.com>
2018-10-01 03:09:19 -0600

    Storyboard Configuration: HomeListController

---
+++ Home News/Home News/controller/HomeListController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeListController.swift
+//  Home News
+//
+//  Created by Jason Sanchez on 9/30/18.
+//  Copyright © 2018 Jason Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class HomeListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Home News/Home News/controller/SaleHistoryController.swift
@@ -0,0 +1,30 @@
+//
+//  SaleHistoryController.swift
+//  Home News
+//
+//  Created by Jason Sanchez on 9/30/18.
+//  Copyright © 2018 Jason Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class SaleHistoryController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
